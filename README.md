6bb1d9bda3c32953667846ca3ca4101da93e8966
Eric Lee <hownl96@gmail.com>
2018-05-22 02:18:29 -0500

    Added Description and code for the project

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # MyMegabusTicketsToExcelSpreadSheet
-A JavaScript script that takes the HTML of the 'My Tickets' at https://us.megabus.com/account-management/my-tickets and prints out the raw data of the tickets to paste into excel.
+![Excel Spreadsheet](Images\FinishedProduct.png)
+
+A JavaScript script that takes the HTML of the Megabus's ['My Tickets'](https://us.megabus.com/account-management/my-tickets) and prints out the raw data of the tickets to paste into excel.
+
+
+## How to use
+
+1. Create an Excel Spreadsheet
+
+2. Create different Exel Tabs for tickets (optional)
+![Sheet Tabs Format](Images\SheetTabsFormat.png)
+
+3. Format the labels for the info given like this:
+![Label Format](Images\LabelFormat.png)
+
+4. Go to [Megabus's My Tickets](https://us.megabus.com/account-management/my-tickets) page and right click to access 'Inspect'
+![Inspect Page](Images\Inspect.png)
+
+5. Go to the console tab in 'Inspect', and paste the JavaScript code into the console. Press Enter.
+
+6. Copy the text and paste it into cell 'A2' of your Excel spreadsheet
+
+7. Format as necessary.
+
+
+## Underlying code description
+
+Basically, the code looks for important elements in the page based off of the class names, ex. ticket__price__item, ticket__reservation__no, etc... The code then takes the elements, gets the values inside of the element, and with some formatting, creates a string, which at the end is concatenated together to create a ticket.
+
+These tickets are compiled into a variable that contains all the tickets.
+
+I've made it so that you can also get a string of the 'Current Tickets' and 'Past Tickets' so you can keep track of previous expenditures or take note of how much you've paid currently.
+
+NOTE: This does not keep track of the batches you bookes, nor how much is spent. It can only get the value of the tickets.
+
+... will add more description later... hopefully
+
+
---
+++ megabustoexcel.js
@@ -0,0 +1,81 @@
+total = ''
+parser = new DOMParser();
+var txt = document.documentElement.innerHTML;
+htmlDoc=parser.parseFromString(txt, "text/html");
+
+htmlDoct = htmlDoc.getElementsByClassName('btn btn-default btn-copy')[0].remove();
+ticketsSections = htmlDoc.getElementsByClassName('row account-mytickets-section');
+
+count = 0;
+for (var i=0; i < ticketsSections.length; i++) {
+	part = ''
+	if (count == 0) {
+		console.log('Current Tickets\n------------------------');
+		// total += 'Current Tickets\n------------------------';
+	} else {
+		console.log('\nPast Tickets\n------------------------');
+		// total += '\nPast Tickets\n------------------------';
+	}
+	ticketElems = ticketsSections[i].getElementsByClassName('ticket--basket');
+	for (var j=0; j < ticketElems.length; j++) {
+		ticketString = ''
+		ticket = ticketElems[j];
+
+
+		// Booking Time of the ticket
+		bookingTimes = ticket.getElementsByClassName('row ticket__booking-time');
+		dateString = '';
+		timeString = '';
+		for (var x=0; x < bookingTimes.length; x++) {
+			partsElem = bookingTimes[x].getElementsByClassName('col-xs-6');
+			for (var k=0; k < partsElem.length; k++) {
+				dateTimeString = '';
+				partElem = partsElem[k];
+				bookingTimeElems = partElem.getElementsByClassName('ticket__booking-time__item');
+
+				dateString = bookingTimeElems[0].textContent.trim() + '\t';
+				timeString += bookingTimeElems[1].textContent.trim() + '\t';
+			}
+		}
+
+		// Origin and Destination of ticket
+		times = ticket.getElementsByClassName('col-xs-12 ticket__stops');
+		timeElem = times[0].getElementsByClassName('ticket__stops__item');
+		originString = timeElem[0].textContent.trim() + '\t';
+		destString = timeElem[1].textContent.trim() + '\t';
+
+
+		// Price of the ticket
+		priceElem = ticket.getElementsByClassName('ticket__price__item');
+		priceString = priceElem[0].textContent.replace(/ /g,'') + '\t';
+
+		// Reservation Number of ticket
+		reservationNumberElem = ticket.getElementsByClassName('ticket__reservation__no');
+		reservationNumberString = reservationNumberElem[0].textContent.replace(/ /g,'').replace('Copy', '') + '\t';
+
+
+		// Concatenating all the data together
+		ticketString += dateString;
+		
+		ticketString += originString;
+		ticketString += destString;
+
+		ticketString += timeString;
+
+		ticketString += priceString;
+
+		ticketString += reservationNumberString
+
+		part += ticketString + '\n';
+		// total += '\n==========================\n';
+
+	}
+	count++;
+	console.log(part);
+	total += part;
+}
+console.log('============');
+console.log(total);
+
+
+
